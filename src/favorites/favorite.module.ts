79b25ba14cb9a182a182ac0a5e63fac339d2676d
YuliyaMinsk <yuliya.rostova@gmail.com>
2022-07-23 12:47:35 +0300

    feat: start new task

--- src/favorites/favorite.module.ts
+++ src/favorites/favorite.module.ts
@@ -1,9 +1,9 @@
-import { Module, forwardRef } from "@nestjs/common";
-import { AlbumModule } from "src/albums/album.module";
-import { ArtistModule } from "src/artists/artist.module";
-import { TrackModule } from "src/tracks/track.module";
-import { FavoriteController } from "./favorite.controller";
-import { FavoriteService } from "./favorite.service";
+import { Module, forwardRef } from '@nestjs/common';
+import { AlbumModule } from 'src/albums/album.module';
+import { ArtistModule } from 'src/artists/artist.module';
+import { TrackModule } from 'src/tracks/track.module';
+import { FavoriteController } from './favorite.controller';
+import { FavoriteService } from './favorite.service';
 
 @Module({
   controllers: [FavoriteController],
